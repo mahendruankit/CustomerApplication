fc012f05fa0bf418531468ef32a4cf469df6fabd
Ankit Mahendru <ankit.mahendru@gmail.com>
2022-02-08 15:42:24 +0000

    add styling

--- src/CustomerApp/Home/CustomerApp.MasterPageComponent.ts
+++ src/CustomerApp/Home/CustomerApp.MasterPageComponent.ts
@@ -3,5 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './CustomerApp.MasterPageView.html',
+  styleUrls: ['./CustomerApp.MasterPageView.scss'],
 })
-export class MasterPageComponent {}
+export class MasterPageComponent {
+  currentYear: number = new Date().getFullYear();
+}
